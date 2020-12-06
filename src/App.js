d7c4cadb7798e99a8a76e899a762a9e4ffeb65e4
Yuki Peters <yup@duke.edu>
2020-12-06 10:59:39 +0900

    Renamed Home to Start

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import Datatable from "./components/Datatable/Datatable";
-import Home from "./components/Home/Home";
+import Start from "./components/Start/Start.js";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home}  />
+      <Route exact path="/" component={Start}  />
       <Route path="/Signup" component={Signup} />
       <Route path="/Login" component={Login} />
       <Route path="/Overview" component={Datatable} />
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import "./Home.css";
+import "./Start.css";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export default class Home extends Component {
+export default class Start extends Component {
   render() {
     return (
-      <Container className="home-container">
+      <Container className="start-container">
         <Row>
           <Col
             sm={{ span: 12 }}
@@ -20,7 +20,7 @@ export default class Home extends Component {
               <p className="get-started-text">Empowering people in groups
               <br />Finding members to work together and make their voices count</p>
             </div>
-            <div className="d-flex flex-column home-card p-3 p-lg-5">
+            <div className="d-flex flex-column start-card p-3 p-lg-5">
               <div>
                 <Link to="/login">
                   <Button block size="lg" className="login-btn">
@@ -28,13 +28,13 @@ export default class Home extends Component {
                   </Button>
                 </Link>
                 <Link to="/signup">
-                  <Button block size="lg" className="home-btn">
+                  <Button block size="lg" className="start-btn">
                     <span>Sign Up</span>
                   </Button>
                 </Link>
                 <br/>
                 <Link to="/overview">
-                <Button block size="lg" className="home-btn">
+                <Button block size="lg" className="start-btn">
                     <span>Overview</span>
                   </Button>
                 </Link>
