1ba3642ae4d6b2a026e836d77302c97fddcbe291
rohanpahwa27 <rohan.pahwa27@gmail.com>
2020-12-12 00:08:25 -0500

    signup changes

--- src/components/Signup/SignupCard/SignupCard.js
+++ src/components/Signup/SignupCard/SignupCard.js
@@ -3,7 +3,9 @@ import "./SignupCard.css";
 import queryString from 'query-string';
 import api from "../../../Api/api.js";
 import { Link } from "react-router-dom";
-
+import { withRouter } from "react-router-dom";
+import InputField from "@kiwicom/orbit-components/lib/InputField";
+import Button from "@kiwicom/orbit-components/lib/Button";
 
 const initialState = {
   email: "",
@@ -11,53 +13,52 @@ const initialState = {
   confirmPassword: "",
   firstName: "",
   lastName: "",
-  orgCode: "",
-  errors: []
+  org: "",
+  errors: {}
 };
 
-function validate(email, password, confirmPassword, firstName, lastName) {
-  const errors = [];
+function validate(email, password, confirmPassword, firstName, lastName, org) {
+  const errors = {};
   if (firstName.length === 0) {
-    errors.push("First Name can't be empty");
-    return errors;
+    errors.firstName = "Required Field"
   }
   if (lastName.length === 0) {
-    errors.push("Last Name can't be empty");
-    return errors;
+    errors.lastName = "Required Field"
   }
 
   if (email.length === 0) {
-    errors.push("Email can't be empty");
-    return errors;
+    errors.email = "Required Field"
   }
 
   if (!email.includes('@')){
-    errors.push("Email must include \'@\' symbol")
-    return errors;
+    errors.email = "Email must include \'@\' symbol"
   }
 
   if (password.length < 6) {
-    errors.push("Password should be at least 6 characters long");
-    return errors;
+    errors.password = "Must be 6+ characters"
+  }
+
+  if (org.length === 0) {
+    errors.org = "Required Field"
   }
 
   if (password != confirmPassword) {
-    errors.push("Passwords don't match.");
-    return errors;
+    errors.password = "Passwords don't match."
+    errors.confirmPassword = " "
   }
 
-  return [];
+  return errors;
 }
-export default class SignupCard extends React.Component {
-  constructor() {
-    super();
+class SignupCard extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = initialState;
   }
   
   handleSubmit = async (event, admin) => {
     event.preventDefault();
     console.log('ADMIN HERE:', admin)
-    const errors = validate(this.state.email, this.state.password, this.state.confirmPassword, this.state.firstName, this.state.lastName);
+    const errors = validate(this.state.email, this.state.password, this.state.confirmPassword, this.state.firstName, this.state.lastName, this.state.org);
     console.log(errors);
     if (errors.length > 0) {
       this.setState({ errors: errors });
@@ -67,12 +68,14 @@ export default class SignupCard extends React.Component {
       email: this.state.email,
       password: this.state.password,
       firstName: this.state.firstName,
-      lastName: this.state.lastName
+      lastName: this.state.lastName,
+      admin: admin
     }
+
     if (admin) {
-      user.organizationName = this.state.orgCode;
+      user.organizationName = this.state.org;
     } else {
-      user.organizationCode = this.state.orgCode;
+      user.organizationCode = this.state.org;
     }
     const response = await api.insertUser(user);
 
@@ -81,7 +84,10 @@ export default class SignupCard extends React.Component {
       this.setState({ errors: errors });
       return;
     }
-    this.setState(initialState);
+
+    localStorage.setItem("userID", response.data.user._id);
+    localStorage.setItem("orgID", response.data.user.organizations[0]);
+    this.props.history.push("/Login");
   };
 
   handleEmailChange = evt => {
@@ -104,8 +110,8 @@ export default class SignupCard extends React.Component {
     this.setState({ lastName: evt.target.value });
   };
 
-  handleOrgCode = evt => {
-    this.setState({ orgCode: evt.target.value });
+  handleOrg = evt => {
+    this.setState({ org: evt.target.value });
   };
 
   render() {
@@ -115,13 +121,106 @@ export default class SignupCard extends React.Component {
     let submit;
     let signup;
     if (!admin) {
-      submit = <input type="submit" value="Done" admin={false} onClick={(e) => this.handleSubmit(e, admin)}></input>
-      signup = <p className="adminSignUp">if your group doesn’t have an add code or any accounts with us yet, 
-      <Link className='signupLink' to="/signup?query=admin"> sign up as a administrator </Link></p>
+      // submit = <input type="submit" value="Done" admin={false} onClick={(e) => this.handleSubmit(e, admin)}></input>
+      submit = <Button type="primary" admin={false} onClick={(e) => this.handleSubmit(e, admin)}>Done</Button>
+      signup = <p className="adminSignUp">if your group doesn’t have an add code or any accounts with us yet, <Link className='signupLink' to="/signup?query=admin">sign up as a administrator </Link></p>
     }
     return (
       <div className="signup-container">
-        <div className = "col1">
+        <div className="signup-card">
+          <div class="row">
+            <div class="column">
+              <InputField
+                // className="signup-input"
+                label="First Name"
+                placeholder="type your first name"
+                error = {this.state.errors.firstName}
+                value={this.state.firstName}
+                onChange={this.handleFirstNameChange}
+              /> 
+            </div>
+            <div class="column">
+              <InputField
+                // className="signup-input"
+                label="Last Name"
+                placeholder="type your last name"
+                error = {this.state.errors.lastName}
+                value={this.state.lastName}
+                onChange={this.handleLastNameChange}
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="column">
+              <InputField
+                className="signup-input"
+                label="Email"
+                error = {this.state.errors.email}
+                placeholder="type your email"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+              />
+            </div>
+            <div class="column"></div>
+          </div>
+          <div class="row">
+            <div class="column">
+              <InputField
+                className="signup-input"
+                label="Password"
+                placeholder="create a password"
+                type="password"
+                help="6+ characters"
+                error={this.state.errors.password}
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+              />
+            </div>
+            <div class="column">
+              <InputField
+                className="signup-input"
+                label="Confirm Password"
+                placeholder="retype password"
+                type="password"
+                error={this.state.errors.confirmPassword}
+                value={this.state.confirmPassword}
+                onChange={this.handleConfirmPasswordChange}
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="column">
+            <InputField
+              className="signup-input"
+              label="Add Code"
+              placeholder= {memberIsAdmin}
+              error={this.state.errors.org}
+              value={this.state.org}
+              onChange={this.handleOrg}
+            />
+            </div>
+            <div class="column">
+              {signup}
+            </div>
+          </div>
+          <div className="row">
+            <div class="column"></div>
+            <div class="column">
+              {submit}
+            </div>
+          </div>
+        </div>
+      </div>
+          
+        
+    );
+  }
+}
+
+export default withRouter(SignupCard);
+
+
+{/* <div className = "col1">
         <div class="row">
           <div class="column">
             <p className="name">Hatch</p>
@@ -129,10 +228,10 @@ export default class SignupCard extends React.Component {
         </div>
           <div class="row">
             <div class="column">
-              <p className="helper">First Name</p>
-              <input
+              <InputField
                 id="firstNameInput"
                 type="text"
+                label= "First Name"
                 className="signup-input"
                 value={this.state.firstName}
                 placeholder="type your first name"
@@ -194,8 +293,8 @@ export default class SignupCard extends React.Component {
                 type="text"
                 className="signup-input"
                 placeholder= {memberIsAdmin}
-                value={this.state.orgCode}
-                onChange={this.handleOrgCode}
+                value={this.state.org}
+                onChange={this.handleOrg}
               />
             </div>
             <div class="column">
@@ -219,10 +318,4 @@ export default class SignupCard extends React.Component {
             <p className="set-up-account">Set up your account</p> 
               <p className="instructions">We’re so excited to have you here with us!<br></br>Follow these steps to sign up as a member of your organization.</p>
           </div>
-        </div>
-      </div>
-          
-        
-    );
-  }
-}
+        </div> */}
