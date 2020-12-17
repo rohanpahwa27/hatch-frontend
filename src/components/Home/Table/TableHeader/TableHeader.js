6c958f7a5c88465e6d336e4cd05f42fa4080d915
Yuki Peters <yuki.peters@gmail.com>
2020-12-17 18:13:07 +0900

    Text in table column header becomes bold when column header is selected

--- src/components/Home/Table/TableHeader/TableHeader.js
+++ src/components/Home/Table/TableHeader/TableHeader.js
@@ -12,22 +12,22 @@ class TableHeader extends Component {
         return (
             <thead id="table-header-grid-container">
                 <tr id="table-header-row-grid-container">
-                    <th id="name-header" className="name" onClick={event => this.props.handleSort(event)}>
-                        <span className="name">Name</span> 
+                    <th id="name-header" className="name-header-click-area" onClick={event => this.props.handleSort(event)}>
+                        <span className="column-header-span name-header-click-area" style={sortBy === "name" ? {fontWeight: "bold"} : null}>Name</span> 
                         {sortBy === "name" ? 
-                        <img className="sort-arrow name" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null}  
+                        <img className="sort-arrow name-header-click-area" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null}  
                     </th>
 
-                    <th id="likes-header" className="likes" onClick={event => this.props.handleSort(event)}>
-                        <span className="likes">Likes</span>
+                    <th id="likes-header" className="likes-header-click-area" onClick={event => this.props.handleSort(event)}>
+                        <span className="column-header-span likes-header-click-area" style={sortBy === "likes" ? {fontWeight: "bold"} : null}>Likes</span>
                         {sortBy === "likes" ? 
-                        <img className="sort-arrow likes" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null} 
+                        <img className="sort-arrow likes-header-click-area" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null} 
                     </th>
 
-                    <th id="comments-header" className="comments" onClick={event => this.props.handleSort(event)}>
-                        <span className="comments">Comments</span>
+                    <th id="comments-header" className="comments-header-click-area" onClick={event => this.props.handleSort(event)}>
+                        <span className="column-header-span comments-header-click-area" style={sortBy === "comments" ? {fontWeight: "bold"} : null}>Comments</span>
                         {sortBy === "comments" ? 
-                        <img className="sort-arrow comments" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null}                 
+                        <img className="sort-arrow comments-header-click-area" src={sortDirection === "ascending" ? upArrow : downArrow} alt="Sort arrow" /> : null}                 
                     </th>
 
                     <th id="tags-header">Tags</th>
