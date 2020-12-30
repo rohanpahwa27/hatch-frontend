0b1b405828c957e1e30d85ceb901e995ec7b6daa
LeoAuyeung <leo297747@gmail.com>
2020-12-29 23:04:12 -0500

    Finished adding Navbar to manage applicants page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import "./Navbar.css";
+import hatchLogo from "./Assets/hatch.png";
 import { Button, Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+// import Logo from "../Home/Logo/Logo.js";
 
 class Navbar extends Component {
   render() {
     return (
-      <div id="grid-container">
-        <div id="grid-item-1">hi</div>
-        <div id="grid-item-2">hi</div>
-        <div id="grid-item-3">hi</div>
-        <div id="grid-item-4">hi</div>
-        <div id="grid-item-5">hi</div>
+      <div id="navbar-flex-container">
+        <div id="navbar-logo-item">
+          <img id="navbar-logo" src={hatchLogo} alt="Hatch" />
+        </div>
+        <div id="navbar-administration-text-item">
+          <span id="navbar-administration-text">Administration</span>
+        </div>
+        <div id="navbar-applicants-link-item">
+          <NavLink
+            id="navbar-link"
+            to="/manage"
+          >Applicants</NavLink>
+        </div>
+        <div id="navbar-members-link-item">
+          <NavLink
+            id="navbar-link"
+            to="/manageMembers"
+          >Members</NavLink>
+        </div>
       </div>
     )
   }
