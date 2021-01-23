8c9342ad4c63a7e7b11a206bbc03acf093bedd68
rohanpahwa27 <rohan.pahwa27@gmail.com>
2021-01-22 20:40:31 -0500

    test

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,9 +2,14 @@ import axios from "axios";
 const FileDownload = require('js-file-download');
 
 const api = axios.create({
-  baseURL: "https://hatchrecruiting-service.herokuapp.com/",
+  baseURL: "https://hatchrecruiting-service.herokuapp.com/:5000",
   withCredentials: true
 });
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000",
+//   withCredentials: true
+// });
 
 // Organizations
 const getAllOrgs = () => api.get("/organizations")
