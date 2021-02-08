1c4168f9df59efc504f1347b88f9397b362eb4e5
LeoAuyeung <leo297747@gmail.com>
2021-02-07 21:01:51 -0500

    Minor changes on manage applicant table headers

--- src/components/Admin/ManageApplicants/TableToolbar/ShowingApplicantsLabel/ShowingApplicantsLabel.js
+++ src/components/Admin/ManageApplicants/TableToolbar/ShowingApplicantsLabel/ShowingApplicantsLabel.js
@@ -5,7 +5,12 @@ class ShowingApplicantsLabel extends Component {
     render() {
         return (
             <div id="toolbar-info-text">
-                <span>Showing {this.props.numApplicantsShowing} of {this.props.totalApplicants} applicants</span>
+                {
+                    this.props.numApplicantsShowing == this.props.totalApplicants ?
+                    <span>Showing all ({this.props.totalApplicants})</span>
+                    :
+                    <span>Showing {this.props.numApplicantsShowing} of {this.props.totalApplicants} applicants</span>
+                }
             </div>
         )
     }
