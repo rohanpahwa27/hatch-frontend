b96a49332bb3b206e37d121d89a4fb0a4b133a7f
rohanpahwa27 <rohan.pahwa27@gmail.com>
2021-02-28 22:16:33 -0500

    updating baseurl

--- src/Api/api.js
+++ src/Api/api.js
@@ -5,7 +5,7 @@ const FileDownload = require('js-file-download');
 //   baseURL: "https://hatchrecruiting-service.herokuapp.com/",
 //   withCredentials: true
 // });
-const BASEURL = (process.env.NODE_ENV == 'production') ? "https://hatchrecruiting-backend.herokuapp.com/": "http://localhost:3000"
+const BASEURL = (process.env.NODE_ENV == 'production') ? "https://hatchrecruiting-backend.herokuapp.com": "http://localhost:3000"
 const api = axios.create({
   baseURL: BASEURL,
   withCredentials: true
